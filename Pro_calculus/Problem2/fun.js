966bdc6f006390820bfa60049017dd82b600f96f
BodanChen <18377475@buaa.edu.cn>
2021-09-06 12:14:52 +0800

    add Problem2

---
+++ Pro_calculus/Problem2/fun.js
@@ -0,0 +1,14 @@
+/*
+ * @author: Bodan Chen
+ * @Date: 2021-09-06 11:45:43
+ * @LastEditors: Bodan Chen
+ * @LastEditTime: 2021-09-06 12:06:52
+ * @Email: 18377475@buaa.edu.cn
+ */
+"use strict";
+$(function() {
+    $("#testbutton").click(function() {
+        //$("#topcards").attr("display") = "none";
+        $("#topcards").fadeIn("slow");
+    })
+});
